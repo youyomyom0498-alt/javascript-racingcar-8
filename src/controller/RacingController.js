9aefc0ed9dc3bf3007a435e1568a1020ededea27
youyomyom0498-alt <youyomyom@gmail.com>
2025-10-25 16:32:21 +0900

    feat(Car): 무작위 값이 4 이상인지 확인해 자동차 전진 여부를 판단하는 기능 추가

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -12,5 +12,10 @@ export class RacingController {
         const tryCountInput = await this.view.inputTryCount();
 
         this.game = new RacingGame(carInput.split(','), Number(tryCountInput));
+
+        for(let i = 0; i < this.game.carList.length; i++){
+            this.game.carList[i].move();
+        }
+        
     }
 }
--- src/model/Car.js
+++ src/model/Car.js
@@ -1,6 +1,19 @@
+import { Random } from "@woowacourse/mission-utils";
+
 export class Car {
     constructor(name) {
         this.name = name;
         this.position = 0;
+    }
+
+    move() {
+        const randomNumber = Random.pickNumberInRange(0, 9);
+        let move = false;
+        if(randomNumber >= 4){
+            move = true;
+        } else {
+            move = false;
+        }
+        return move;
     }
 }
