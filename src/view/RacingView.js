5434d098069b7fff1da2145b1fdefeab761c5e6a
youyomyom0498-alt <youyomyom@gmail.com>
2025-10-26 17:29:38 +0900

    refactor(view): 출력 형식 기준에 맞추어 수정(입력 부분)

--- src/view/RacingView.js
+++ src/view/RacingView.js
@@ -5,14 +5,14 @@ export class RacingView{
 
     async inputCarNames() {
         const nameInput = await Console.readLineAsync(
-            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
         );
         return nameInput;
     }
 
     async inputTryCount() {
         const tryCountInput = await Console.readLineAsync(
-            '시도할 횟수는 몇 회인가요?'
+            '시도할 횟수는 몇 회인가요?\n'
         );
         return tryCountInput;
     }
